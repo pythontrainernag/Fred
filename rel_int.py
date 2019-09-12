0757e30b6689627aabde0d4937ffad85d80e2188
NageswaraRao <pythontrainernag@gmail.com>
2019-09-12 07:31:19 +0530

    12/09/2019

---
+++ rel_int.py
@@ -0,0 +1,11 @@
+#Relational operators
+#I will always try to compare values
+#>, <, >=, <=, ==, !=
+#The outcome relational operator is always boolean(True/False)
+print("3>2 is", 3>2)# value must be greater to get the outcome as a True
+print("3<2 is", 3<2)#False
+print("3>=2 is", 3>=2)# >= ---> Value can be greater or equals to get the outcome as True
+print("3>=3 is", 3>=3)
+print("3<=2 is", 3<=2)
+print("3==2 is", 3==2)
+print("3!=2 is", 3!=2)
