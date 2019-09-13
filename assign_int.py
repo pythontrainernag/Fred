d82d54a5d9892328e845767b9e2f5c3092e6a754
NageswaraRao <pythontrainernag@gmail.com>
2019-09-13 06:54:49 +0530

    13/09/2019

---
+++ assign_int.py
@@ -0,0 +1,19 @@
+# Assignment operators
+# =, +=, -=, *= ----> similarly with every arthematic operators
+# Assignment operators are used for assigning a value/values to a variable
+# This operators can't be used along with print statements
+
+a = 5 # The value(5) will be assigned to a valriable "a"
+print(a)#As "a" value is 5, it prints 5
+#print(a=5)
+
+a += 10 # a = a+10 Always right side of the = is executed first and result is assigned to variable on left side
+print(a)
+
+a *= 4 # a = a*4
+print(a)
+
+# modulos is nothing but a remainder
+a %= 8# a = a%8 ---> 60%8
+print(a)
+
---
+++ log_int.py
@@ -0,0 +1,14 @@
+# Logical operators
+# and, or, not
+# and, or are binary operators because it requires two values to action
+# not is unary operator because it requires single value
+# while converting integers to boolean, all numbers except zero will be converted as True and zero will be converted as False
+# Generally logical operators will accept only boolean values as input.
+# In case of python, Even if I provide integers then python will Automatically convert them to boolean and perform the action
+# Even though the outputs and inputs are in integers, we have understand them in boolean format only
+
+print("5 and 3 is", 5 and 3)# both inputs are True, so output is also True
+print("5 and 0 is", 5 and 0)# 5 is True, 0 is False. So output is False(0)
+print("5 or 0 is", 5 or 0)
+print("0 or 0 is", 0 or 0)
+
